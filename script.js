c56b357626fb2f85d3da04885a7cdaa4dd5a5b3a
Pratyush <choudharypratyush809@gmail.com>
2024-10-20 21:21:06 +0530

    color changes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// import Chart from 'chart.js/auto';
+
 let sizeArray = 20;
 let array = [sizeArray];
 let sortedarray = [sizeArray];
@@ -17,34 +19,46 @@ function randomValue(){
 
 function newArray(){
     generateArray();
-    chart.data.datasets[0].data = 'array';
+    chart.data.datasets[0].data = array;
     chart.update();
 }
 
 // bubble sort algo
-function bubblesort(){
+async function bubblesort(){
         sortedarray = Array.from(array);  // copied sortedarray from array
-        for(let i=0; i< sizeArray; i++){
+        for(let i=0; i< sizeArray-1; i++){
             for(let j=0; j< sizeArray-1-i; j++){
+               // Highlight current elements being compared
+            colors[j] = 'red';
+            colors[j + 1] = 'red';
+            chart.update();
+            await new Promise(resolve => setTimeout(resolve, 800)); // Delay for visualization
                 if(sortedarray[j]> sortedarray[j+1]){
                     [sortedarray[j], sortedarray[j+1]] = [sortedarray[j+1], sortedarray[j]];
                 }
+                // Reset colors
+            colors[j] = '#4bc0c0';
+            colors[j + 1] = '#4bc0c0';
             }
+            colors[sizeArray - i - 1] = 'green'; // Color sorted elements
         }
-    
+    // Final update to mark the last sorted element
+    colors[0] = 'green'; 
+    chart.update();
     console.log(sortedarray);
 }
-function sortArray(){
+function sortArray(array){
     bubblesort();
-    chart.data.datasets[0].data = 'sortedarray';
+    chart.data.datasets[0].data = sortedarray;
     chart.update();
 }
+
+
 // Bar chart
-
-  let ctx = document.getElementById('myChart').getContext("2d");
-
-  var chart = new Chart(ctx, {
-    type: 'bar',
+  let myChart = document.getElementById('myChart').getContext("2d");
+  const colors = array.map(() => '#4bc0c0'); // Default color
+  let chart = new Chart(myChart, {
+    type: "bar",
     data: {
       labels: [
         "Element 1",
@@ -73,14 +87,18 @@ function sortArray(){
         data: array,
         fill: false,
         borderColor: "009688",
-        backgroundcolor: "#FFF",
-        borderWidth: 5,
+        backgroundColor: colors,
+        borderWidth: 1,
       },
     ],
     },
     options: {
         reponsive: true,
         maintainAspectRatio: false,
+        animation: {
+          duration: 2200
+        },
+
       scales: {
         y: {
             ticks: {
@@ -89,7 +107,7 @@ function sortArray(){
           },
         y: {
           ticks: {
-            color: "white",
+            color: "black",
           },
         },
       },
